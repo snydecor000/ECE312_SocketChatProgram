33c32f9ac42a382323ffd00863762e05f5d8cf01
snydecor000 <cory.a.snyder@gmail.com>
2020-09-15 16:57:41 -0400

    Ask for the port in the server program rather than having it as an arg

--- ChatServer.c
+++ ChatServer.c
@@ -5,40 +5,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(char *msg)
-{
+#define BUFFER_LEN 256 //Defines how long a message can be
+
+void error(char *msg) {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, newsockfd, portno, clilen;
-    char buffer[256];
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, clilen;
+    int portno = 0;
+    char buffer[BUFFER_LEN];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
+
+    while(portno == 0) {
+        printf("Enter a port # to listen to: ");
+        bzero(buffer, BUFFER_LEN);
+        fgets(buffer, BUFFER_LEN-1, stdin);
+        portno = atoi(buffer);
+        if(portno < 2000 || portno > 65535) {
+            portno = 0;
+            printf("Please enter a valid port # from 2000 to 65535\n");
+        }
     }
+
+    //Attempt to open the socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+
+    //If there was an error opening the socket
+    if (sockfd < 0) {
         error("ERROR opening socket");
+    }
+
+    //Clear the server address, and put all the neccesary info in it
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                sizeof(serv_addr)) < 0) 
-                error("ERROR on binding");
+
+    //Attempt to bind with the socket
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR on binding");
+    }
+
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) 
-            error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
+    if (newsockfd < 0) {
+        error("ERROR on accept");
+    }
+    bzero(buffer,BUFFER_LEN);//Clear Buffer
+    n = read(newsockfd,buffer,BUFFER_LEN-1);//Fill Buffer with the message
     if (n < 0) error("ERROR reading from socket");
     printf("Here is the message: %s\n",buffer);
     n = write(newsockfd,"I got your message",18);
